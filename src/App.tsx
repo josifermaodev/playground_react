331b90ef4c59a7ccf9ca7c10514136f127bd9ce2
josifermaodev <josianefermao@gmail.com>
2025-02-13 18:23:09 -0300

    Atualização do arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css'
+import Contador from './pages/contador/Contador'
+import Home from './pages/home/Home'
 import Login from './pages/login/Login'
+import Tarefa from './pages/tarefa/Tarefa'
 
 
 function App() {
 
   return (
     <>
+        <Home 
+          titulo={'Seja Bem vindo'} 
+          texto={'Aqui é a a página inicial'} 
+        />
         <Login />
+        <Tarefa />
+        <Contador />
+
     </>
   )
 }
